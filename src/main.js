348b8a445569bc2ad48693ceebd5f53b3b4c9c36
Betsycampos <cbetsydalia@gmail.com>
2018-12-17 08:11:09 -0500

    Añade pueba1 JS

--- src/main.js
+++ src/main.js
@@ -23,7 +23,8 @@
 
 // mostrando el nombre y el titulo en el card n° 1
   const nombres = arrLol.map( arrLol =>  arrLol.name).filter( name => name === "Aatrox");
-  const title = arrLol.map( arrLol =>  arrLol.title).filter( name => name === "the Darkin Blade");;
+  const title = arrLol.map( arrLol =>  arrLol.title).filter( name => name === "the Darkin Blade");
+  
   
   cardTitle.innerHTML = `<h5>${nombres}</h5>`
   cardText.innerHTML = `<p>${title}</p>`
@@ -35,7 +36,13 @@
 
 const ordenarAscendente = function(){
   
-}
+
 const ascedente = document.getElementById("ascendente");
 ascedente.addEventListener("click", ordenarAscendente )
+ }
 console.log(ordenarAscendente); 
+
+//Creando la función del buscador
+
+//const buscandoHeroe = document.getElementById("buscar")
+  
