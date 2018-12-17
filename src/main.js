90bc7be816c4c73a2230e552a1d4b63517e1b516
JoanLazo <joan162581@gmail.com>
2018-12-17 09:29:02 -0500

    comitee cosas en main.js para hacer el marge con betsy

--- src/main.js
+++ src/main.js
@@ -6,8 +6,8 @@
     //    newArrName.push(objData[arrLol[i]].name); 
     // }
     
-  const cardTitle = document.getElementById("nombre");
-  const cardText = document.getElementById("titulo");
+  // const cardTitle = document.getElementById("nombre");
+  // const cardText = document.getElementById("titulo");
 
 
   // Creando una funcion para que se muestre la data en los cards
@@ -19,11 +19,11 @@
   //  };
 
 // mostrando el nombre y el titulo en el card n° 1
-  const nombres = arrLol.map( arrLol =>  arrLol.name).filter( name => name === "Aatrox");
-  const title = arrLol.map( arrLol =>  arrLol.title).filter( name => name === "the Darkin Blade");;
+  // const nombres = arrLol.map( arrLol =>  arrLol.name).filter( name => name === "Aatrox");
+  // const title = arrLol.map( arrLol =>  arrLol.title).filter( name => name === "the Darkin Blade");;
   
-  cardTitle.innerHTML = `<h5>${nombres}</h5>`
-  cardText.innerHTML = `<p>${title}</p>`
+  // cardTitle.innerHTML = `<h5>${nombres}</h5>`
+  // cardText.innerHTML = `<p>${title}</p>`
   
 
 
