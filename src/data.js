4ca229e058be50d9ab7fcd56dae1345967892c89
Betsycampos <cbetsydalia@gmail.com>
2018-12-18 12:52:03 -0500

    Añadiendo cambios en el archivo data.js y main.js

--- src/data.js
+++ src/data.js
@@ -1,32 +1,38 @@
-// esta es una función de ejemplo
-// puedes ver como agregamos la función a nuestro objeto global window
-const objData = LOL.data;
-const arrLol = Object.values(objData);
 
- const ordenarAscendente = () => {
+
+const ordenarAscendente = () => {
   const textoNombre = document.getElementById("nombre");
-  let arrOrdenado = arrLol.map( arrLol => arrLol.name).sort();
+  let arrOrdenado = arrLol.map(arrLol => arrLol.name).sort();
   return textoNombre.innerHTML = arrOrdenado;
- };
+};
 
- 
- const ordenarDescendente = () => {
+
+const ordenarDescendente = () => {
   const textoNombre = document.getElementById("nombre");
-  const arrReverse = arrLol.map( arrLol =>  arrLol.name).reverse();
-  return  textoNombre.innerHTML = arrReverse;
- }
-  
- const inputOrdenar = document.getElementById("ordenar");
- const optionAs = document.getElementById("ascendente");
- const optionDes = document.getElementById("descendente");
- 
- inputOrdenar.addEventListener("click", ordenarAscendente);
+  const arrReverse = arrLol.map(arrLol => arrLol.name).reverse();
+  return textoNombre.innerHTML = arrReverse;
+}
 
- inputOrdenar.addEventListener("change", ordenarDescendente);
+const inputOrdenar = document.getElementById("ordenar");
+const optionAs = document.getElementById("ascendente");
+const optionDes = document.getElementById("descendente");
 
-// console.log(ordenarAscendente());
-// console.log(ordenarDescendente());
+inputOrdenar.addEventListener("click", ordenarAscendente);
+
+inputOrdenar.addEventListener("change", ordenarDescendente);
 
 
 
-window.ordenarAscendente = ordenarAscendente;
+const listaPersonajes = (arr) => {
+  const newArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    newArray.push({ name: arr[i].name, title: arr[i].title, img: arr[i].img, attackrange: arr[i].stats.attackrange });
+    console.log(newArray);
+  }
+
+
+
+  window.lol = {
+    mostrarPersonajes
+  };
+
--- src/main.js
+++ src/main.js
@@ -1,45 +1,12 @@
+const objLol = LOL.data;
+const arrLol = Object.values(objLol);
 
 
-  // let newArrName = [];
-
-    // for(let i=0 ; i < arrLol.length ; i++){
-    //    newArrName.push(objData[arrLol[i]].name); 
-    // }
-    
-  // const cardTitle = document.getElementById("nombre");
-  // const cardText = document.getElementById("titulo");
-
-
-  // Creando una funcion para que se muestre la data en los cards
-  //   const mostrarData = function(){
-  //   for(let i= 0 ; i<arrLol.length; i++){
-    
-  //   }
-    
-  //  };
-
-// mostrando el nombre y el titulo en el card n° 1
-  const nombres = arrLol.map( arrLol =>  arrLol.name).filter( name => name === "Aatrox");
-  const title = arrLol.map( arrLol =>  arrLol.title).filter( name => name === "the Darkin Blade");
+arrLol.forEach(objDatos => {
+  console.log(objDatos)
+});
   
   
-  // cardTitle.innerHTML = `<h5>${nombres}</h5>`
-  // cardText.innerHTML = `<p>${title}</p>`
-  
+let ataque = arrLol.filter(arrLo => arrLo.attack >= 2);
 
 
-
-// creando la funcion de orden ascendente
-
-// const ordenarAscendente = function(){
-  
-
-// const ascedente = document.getElementById("ascendente");
-// ascedente.addEventListener("click", ordenarAscendente )
-//  }
-// console.log(ordenarAscendente); 
-
-//Creando la función del buscador
-
-//const buscandoHeroe = document.getElementById("buscar")
-  
