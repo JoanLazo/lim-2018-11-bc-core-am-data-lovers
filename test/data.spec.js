4de197272adb0469ff6c00de9136f6f161bd2741
Betsycampos <cbetsydalia@gmail.com>
2018-12-20 08:49:51 -0500

    Añadiendo perfección al test: orden y coherencia

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,44 +1,95 @@
 require('../src/data.js');
 
 const input = [
-  { name: 'Aatrox', title: 'the Darkin Blade', info: { attack: 8, defense: 4, magic: 3, difficulty: 4 }, tags: ['Fighter', 'Tank'] },
-  { name: 'Bard', title: 'the Wandering Caretaker', info: { attack: 4, defense: 4, magic: 5, difficulty: 9 }, tags: ['Support', 'Mage'] },
-  { name: 'Caitlyn', title: 'the Sheriff of Piltover', info: { attack: 8, defense: 2, magic: 2, difficulty: 6 }, tags: ['Marksman'] },
-  { name: 'Darius', title: 'the Hand of Noxus', info: { attack: 9, defense: 5, magic: 1, difficulty: 2 }, tags: ['Fighter', 'Tank'] }
+  {
+    splash: 'http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Aatrox_0.jpg',
+    name: 'Aatrox', title: 'the Darkin Blade', info: { attack: 8, defense: 4, magic: 3, difficulty: 4 }, tags: ['Fighter', 'Tank']
+  },
+  {
+    splash: 'http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Bard_0.jpg',
+    name: 'Bard', title: 'the Wandering Caretaker', info: { attack: 4, defense: 4, magic: 5, difficulty: 9 }, tags: ['Support', 'Mage']
+  },
+  {
+    splash: 'http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Caitlyn_0.jpg',
+    name: 'Caitlyn', title: 'the Sheriff of Piltover', info: { attack: 8, defense: 2, magic: 2, difficulty: 6 }, tags: ['Marksman']
+  },
+  {
+    splash: 'http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Darius_0.jpg',
+    name: 'Darius', title: 'the Hand of Noxus', info: { attack: 9, defense: 5, magic: 1, difficulty: 2 }, tags: ['Fighter', 'Tank']
+  }
 ];
 const output1 = [
-  { name: 'Aatrox', title: 'the Darkin Blade', info: { attack: 8, defense: 4, magic: 3, difficulty: 4 }, tags: ['Fighter', 'Tank'] },
-  { name: 'Bard', title: 'the Wandering Caretaker', info: { attack: 4, defense: 4, magic: 5, difficulty: 9 }, tags: ['Support', 'Mage'] },
-  { name: 'Caitlyn', title: 'the Sheriff of Piltover', info: { attack: 8, defense: 2, magic: 2, difficulty: 6 }, tags: ['Marksman'] },
-  { name: 'Darius', title: 'the Hand of Noxus', info: { attack: 9, defense: 5, magic: 1, difficulty: 2 }, tags: ['Fighter', 'Tank'] },
+  {
+    splash: 'http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Aatrox_0.jpg',
+    name: 'Aatrox', title: 'the Darkin Blade', info: { attack: 8, defense: 4, magic: 3, difficulty: 4 }, tags: ['Fighter', 'Tank']
+  },
+  {
+    splash: 'http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Bard_0.jpg',
+    name: 'Bard', title: 'the Wandering Caretaker', info: { attack: 4, defense: 4, magic: 5, difficulty: 9 }, tags: ['Support', 'Mage']
+  },
+  {
+    splash: 'http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Caitlyn_0.jpg',
+    name: 'Caitlyn', title: 'the Sheriff of Piltover', info: { attack: 8, defense: 2, magic: 2, difficulty: 6 }, tags: ['Marksman']
+  },
+  {
+    splash: 'http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Darius_0.jpg',
+    name: 'Darius', title: 'the Hand of Noxus', info: { attack: 9, defense: 5, magic: 1, difficulty: 2 }, tags: ['Fighter', 'Tank']
+  },
 ];
 const output2 = [
-  { name: 'Aatrox', title: 'the Darkin Blade', info: { attack: 8, defense: 4, magic: 3, difficulty: 4 }, tags: ['Fighter', 'Tank'] },
-  { name: 'Bard', title: 'the Wandering Caretaker', info: { attack: 4, defense: 4, magic: 5, difficulty: 9 }, tags: ['Support', 'Mage'] },
-  { name: 'Caitlyn', title: 'the Sheriff of Piltover', info: { attack: 8, defense: 2, magic: 2, difficulty: 6 }, tags: ['Marksman'] },
-  { name: 'Darius', title: 'the Hand of Noxus', info: { attack: 9, defense: 5, magic: 1, difficulty: 2 }, tags: ['Fighter', 'Tank'] },
+  {
+    splash: 'http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Aatrox_0.jpg',
+    name: 'Aatrox', title: 'the Darkin Blade', info: { attack: 8, defense: 4, magic: 3, difficulty: 4 }, tags: ['Fighter', 'Tank']
+  },
+  {
+    splash: 'http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Bard_0.jpg',
+    name: 'Bard', title: 'the Wandering Caretaker', info: { attack: 4, defense: 4, magic: 5, difficulty: 9 }, tags: ['Support', 'Mage']
+  },
+  {
+    splash: 'http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Caitlyn_0.jpg',
+    name: 'Caitlyn', title: 'the Sheriff of Piltover', info: { attack: 8, defense: 2, magic: 2, difficulty: 6 }, tags: ['Marksman']
+  },
+  {
+    splash: 'http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Darius_0.jpg',
+    name: 'Darius', title: 'the Hand of Noxus', info: { attack: 9, defense: 5, magic: 1, difficulty: 2 }, tags: ['Fighter', 'Tank']
+  },
 ];
 const output3 = [
-  { splash: 'http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Zyra_0.jpg',
-    name: 'Zyra', title: 'Rise of the Thorns"', info: { attack: 4, defense: 3, magic: 8, difficulty: 7 }, tags: ['Mage', 'Support'] },
-  { name: 'Yorick', title: 'Shepherd of Souls', info: { attack: 6, defense: 6, magic: 4, difficulty: 6 }, tags: ['Fighter', 'Tank'] },
-  { name: 'Xerath', title: 'the Magus Ascendant', info: { attack: 1, defense: 3, magic: 10, difficulty: 8 }, tags: ['Mage', 'Assassin'] },
-  { name: 'Warwick', title: 'the Blood Hunter', info: { attack: 7, defense: 4, magic: 4, difficulty: 3 }, tags: ['Fighter', 'Tank'] },
+  {
+    splash: 'http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Darius_0.jpg',
+    name: 'Darius', title: 'the Hand of Noxus', info: { attack: 9, defense: 5, magic: 1, difficulty: 2 }, tags: ['Fighter', 'Tank']
+  },
+  {
+    splash: 'http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Caitlyn_0.jpg',
+    name: 'Caitlyn', title: 'the Sheriff of Piltover', info: { attack: 8, defense: 2, magic: 2, difficulty: 6 }, tags: ['Marksman']
+  },
+  {
+    splash: 'http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Bard_0.jpg',
+    name: 'Bard', title: 'the Wandering Caretaker', info: { attack: 4, defense: 4, magic: 5, difficulty: 9 }, tags: ['Support', 'Mage']
+  },
+  {
+    splash: 'http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Aatrox_0.jpg',
+    name: 'Aatrox', title: 'the Darkin Blade', info: { attack: 8, defense: 4, magic: 3, difficulty: 4 }, tags: ['Fighter', 'Tank']
+  },
 ];
 const output4 = [
-  { tags: ['Fighter', 'Tank'] },
-  { tags: ['Support', 'Mage'] },
-  { tags: ['Marksman'] },
-  { tags: ['Fighter', 'Tank'] },
+  {
+    splash: 'http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Caitlyn_0.jpg',
+    name: 'Caitlyn', title: 'the Sheriff of Piltover', info: { attack: 8, defense: 2, magic: 2, difficulty: 6 }, tags: ['Marksman']
+  },
+
 ];
 const output5 = [
-  { name: 'Aatrox', title: 'the Darkin Blade', info: {attack: 8, defense: 4, magic: 3, difficulty: 4}, tags: ['Fighter', 'Tank'] },
+  {
+    splash: 'http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Aatrox_0.jpg',
+    name: 'Aatrox', title: 'the Darkin Blade', info: { attack: 8, defense: 4, magic: 3, difficulty: 4 }, tags: ['Fighter', 'Tank']
+  },
 ];
 const output6 = [
-  { name: 'Master Yi', title: 'the Wuju Bladesman', info: { attack: 10 } },
-  { name: 'Darius', title: 'the Hand of Noxus', info: { attack: 9 } },
-  { name: 'Caitlyn', title: 'the Sheriff of Piltover', info: { attack: 8 } },
-  { name: 'Diana', title: 'Scorn of the Moon', info: { attack: 7 } },
+  { splash: 'http://ddragon.leagueoflegends.com/cdn/img/champion/splash/MasterYi_0.jpg"', name: 'Master Yi', title: 'the Wuju Bladesman', info: { attack: 10 } },
+  { splash: 'http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Darius_0.jpg', name: 'Darius', title: 'the Hand of Noxus', info: { attack: 9 } },
+  { splash: 'http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Caitlyn_0.jpg', name: 'Caitlyn', title: 'the Sheriff of Piltover', info: { attack: 8 } },
+  { splash: 'http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Diana_0.jpg', name: 'Diana', title: 'Scorn of the Moon', info: { attack: 7 } },
 ];
 
 describe('dataL', () => {
@@ -86,7 +137,7 @@ describe('dataL', () => {
     it('deberia ser una funcion', () => {
       expect(typeof dataL.filtraDataTags).toBe('function');
     });
-    it('deberia retornar  los tags por categorias; filtrarlas en orden', () => {
+    it('deberia retornar  los tags por categorias; por cada tags elegido', () => {
       expect(dataL.filtraDataTags(input)).toEqual(output4);
     });
     it('deberia retornar un nuevo array,no modificar el original', () => {
@@ -98,7 +149,7 @@ describe('dataL', () => {
     it('deberia ser una funcion', () => {
       expect(typeof dataL.filtrarPorBusqueda).toBe('function');
     });
-    it('deberia retornar  los tags por categorias; filtrarlas por tipo de ataque', () => {
+    it('deberia retornar la carta con la información inicial; según el nombre del campeón puesto en búsqueda; ', () => {
       expect(dataL.filtrarPorBusqueda(input)).toEqual(output5);
     });
     it('deberia retornar un nuevo array,no modificar el original', () => {
@@ -110,7 +161,7 @@ describe('dataL', () => {
     it('deberia ser una funcion', () => {
       expect(typeof dataL.reduceMayorAtaque).toBe('function');
     });
-    it('deberia retornar el ataque mas alto, el que tenga mayor numero de ataques', () => {
+    it('deberia retornar las cartas por información de ataque de manera descendente', () => {
       expect(dataL.reduceMayorAtaque(input)).toEqual(output6);
     });
     it('deberia retornar un nuevo array,no modificar el original', () => {
