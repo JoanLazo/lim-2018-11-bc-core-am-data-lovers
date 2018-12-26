2a17b9dee159d2635ede3387a20b5a46fbf2f685
Ivan <ivanlazo579@gmail.com>
2018-12-25 20:32:18 -0500

    se realizo las funciones de filtrar, ordenar y buscar por normbre, se coloco las funciones en el main.js juntando con el DOM, se mejoro el css colocando un nombre en dorado por cada carta

--- src/data.js
+++ src/data.js
@@ -1,13 +1,14 @@
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
 
-const mostrarPersonajes = (arr) => {
+const newArrayLol = (arr) => {
   return arr.map(obj => Object.assign({}, obj, {
     name: obj.name,
     title: obj.title,
     splash: obj.splash,
     info: obj.info,
     tags: obj.tags,
+    stats: obj.stats,
   }));
 };
 
@@ -22,7 +23,7 @@ const sortData = (data, sortBy, sortOrder) => {
     }
     break;
   case 'descendente':
-    if (sortBy === 'name') {
+    if (sortBy === 'name-rev') {
       listChampionsOrder = data.sort((nameA, nameB) => nameA.name > nameB.name ? 1 : -1).reverse();
     } else {
       listChampionsOrder = data.sort((ataqueA, ataqueB) => ataqueA.info.attack > ataqueB.info.attack ? 1 : -1).reverse();
@@ -44,26 +45,26 @@ const filterData = (data, filterBy, condition) => {
 };
 
 const computeStats = (data) => {
-  let damageIncreased = data.map(obj => obj.stats.attackdamageperlevel - obj.stats.attackdamaga);
+  let damageIncreased = data.map(obj => obj.stats.attackdamage - obj.stats.attackdamageperlevel);
   return damageIncreased;
 };
 
-const filtrarPorBusqueda = (data, value) => {
-  let buscarPorNombre = [];
-  switch (value) {
-  case 'name': 
-    buscarPorNombre = data.filter(champion => champion.name.index(0) === value.toUpperCase() || champion.name === value);
+const filterBySearch = (data, input, value) => {
+  let searchByName = [];
+  switch (input) {
+  case 'value': 
+    searchByName = data.filter(champion => champion.name[0] === value.toUpperCase() || champion.name === value);
     break;
   default:
     alert('Escriba el nombre del campeón que desea buscar');
   }
-  return buscarPorNombre;
+  return searchByName;
 };
 
-window.dataL = {
-  mostrarPersonajes,
+window.dataLol = {
+  newArrayLol,
   sortData,
   filterData,
   computeStats,
-  filtrarPorBusqueda,
+  filterBySearch,
 };
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-const lolDataTotal = window.LOL.data;
+const lolDataTotal = LOL.data;
 const arrLolTotal = Object.values(lolDataTotal);
-const arrLolEspecifico = dataL.mostrarPersonajes(arrLolTotal);
-//const nombresOrdenadosAscendente = dataL.ordenarDataLolAscendente(arrLolEspecifico);
+const arrLolEspecifico = dataLol.newArrayLol(arrLolTotal);
+// Creando las cartas de forma dinamica
 const containerCardTotal = document.getElementById('cards-total');
 const crearCartasLol = (data) => {
   let templateListOfCards = '';
@@ -14,7 +14,7 @@ const crearCartasLol = (data) => {
                     <img class="card-img-top" src="${arrLolEspecifico.splash}"/>
                     <div class="card-body" id="card-body">
                       <h5 class="card-title" id="nombre">${arrLolEspecifico.name}</h5>
-                      <p class="card-text" id="titulo">${arrLolEspecifico.title}</p>
+                      <p class="card-sub-title" id="titulo">${arrLolEspecifico.title}</p>
                       <p class="card-text"> Ataque: ${arrLolEspecifico.info.attack}</p> 
                       <p class="card-text">Defensa: ${arrLolEspecifico.info.defense}</p>
                       <p class="card-text">Magia: ${arrLolEspecifico.info.magic}</p>
@@ -31,6 +31,164 @@ const crearCartasLol = (data) => {
   containerCardTotal.innerHTML = templateListOfCards;
 };
 crearCartasLol(arrLolEspecifico);
-/*const selecOrdenarAscendente = document.getElementById('ascendente');
-const selecOrdenarDescendente = document.getElementById('descendente');
-const buscarCampeonPorNombre = document.getElementById('buscar');*/
+// Ordenando las cartas de forma ascendente y descendente segun el nombre
+const selectOrder = document.getElementById('ordenar');
+selectOrder.addEventListener('change', () => {
+  const sortByname = selectOrder.value;
+  let sortOrder;
+  switch (sortByname) {
+  case 'name':
+    sortOrder = 'ascendente';
+    break;
+  case 'name-rev':
+    sortOrder = 'descendente';
+    break;
+  }
+  const nameOrderAsc = dataLol.sortData(arrLolEspecifico, sortByname, sortOrder);
+  const crearCartasLol = (data) => {
+    let templateListOfCards = '';
+    data.forEach((arrLolEspecifico) => {
+      const card = `
+      <div class="d-flex flex-wrap pt-5" id="contenedor-cartas">
+      <div class="row">
+      <div class="col-sm-3 py-2">
+              <div class="card" style="width: 18rem;">
+                      <img class="card-img-top" src="${arrLolEspecifico.splash}"/>
+                      <div class="card-body" id="card-body">
+                        <h5 class="card-title" id="nombre">${arrLolEspecifico.name}</h5>
+                        <p class="card-sub-title" id="titulo">${arrLolEspecifico.title}</p>
+                        <p class="card-text"> Ataque: ${arrLolEspecifico.info.attack}</p> 
+                        <p class="card-text">Defensa: ${arrLolEspecifico.info.defense}</p>
+                        <p class="card-text">Magia: ${arrLolEspecifico.info.magic}</p>
+                        <p class="card-text">Dificultad: ${arrLolEspecifico.info.difficulty}</p>
+                        <p class="card-text">Tipo: ${arrLolEspecifico.tags}<p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                </div>     
+      `;
+      templateListOfCards += card;
+    });
+    containerCardTotal.innerHTML = templateListOfCards;
+  };
+  crearCartasLol(nameOrderAsc);
+});
+// Ordenando las cartas de forma ascendente y descendente segun el ataque
+const selectOrderAttack = document.getElementById('ataque');
+selectOrderAttack.addEventListener('change', () => {
+  const sortByname = selectOrderAttack.value;
+  let sortOrder;
+  switch (sortByname) {
+  case 'ataque-asc':
+    sortOrder = 'ascendente';
+    break;
+  case 'ataque-desc':
+    sortOrder = 'descendente';
+    break;
+  }
+  const nameOrderAsc = dataLol.sortData(arrLolEspecifico, sortByname, sortOrder);
+  const crearCartasLol = (data) => {
+    let templateListOfCards = '';
+    data.forEach((arrLolEspecifico) => {
+      const card = `
+      <div class="d-flex flex-wrap pt-5" id="contenedor-cartas">
+      <div class="row">
+      <div class="col-sm-3 py-2">
+              <div class="card" style="width: 18rem;">
+                      <img class="card-img-top" src="${arrLolEspecifico.splash}"/>
+                      <div class="card-body" id="card-body">
+                        <h5 class="card-title" id="nombre">${arrLolEspecifico.name}</h5>
+                        <p class="card-sub-title" id="titulo">${arrLolEspecifico.title}</p>
+                        <p class="card-text"> Ataque: ${arrLolEspecifico.info.attack}</p> 
+                        <p class="card-text">Defensa: ${arrLolEspecifico.info.defense}</p>
+                        <p class="card-text">Magia: ${arrLolEspecifico.info.magic}</p>
+                        <p class="card-text">Dificultad: ${arrLolEspecifico.info.difficulty}</p>
+                        <p class="card-text">Tipo: ${arrLolEspecifico.tags}<p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                </div>     
+      `;
+      templateListOfCards += card;
+    });
+    containerCardTotal.innerHTML = templateListOfCards;
+  };
+  crearCartasLol(nameOrderAsc);
+});
+// Filtar los campeones segun el tipo
+const selectByType = document.getElementById('filtro');
+selectByType.addEventListener('change', () => {
+  const filterBy = 'Tags';
+  const championTypeValue = selectByType.value;
+  const championByType = dataLol.filterData(arrLolEspecifico, filterBy, championTypeValue);
+  const crearCartasLol = (data) => {
+    let templateListOfCards = '';
+    data.forEach((arrLolEspecifico) => {
+      const card = `
+      <div class="d-flex flex-wrap pt-5" id="contenedor-cartas">
+      <div class="row">
+      <div class="col-sm-3 py-2">
+              <div class="card" style="width: 18rem;">
+                      <img class="card-img-top" src="${arrLolEspecifico.splash}"/>
+                      <div class="card-body" id="card-body">
+                        <h5 class="card-title" id="nombre">${arrLolEspecifico.name}</h5>
+                        <p class="card-sub-title" id="titulo">${arrLolEspecifico.title}</p>
+                        <p class="card-text"> Ataque: ${arrLolEspecifico.info.attack}</p> 
+                        <p class="card-text">Defensa: ${arrLolEspecifico.info.defense}</p>
+                        <p class="card-text">Magia: ${arrLolEspecifico.info.magic}</p>
+                        <p class="card-text">Dificultad: ${arrLolEspecifico.info.difficulty}</p>
+                        <p class="card-text">Tipo: ${arrLolEspecifico.tags}<p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                </div>     
+      `;
+      templateListOfCards += card;
+    });
+    containerCardTotal.innerHTML = templateListOfCards;
+  };
+  crearCartasLol(championByType);
+});
+// Mostrando los heroes por busqueda 
+
+const btnSearch = document.getElementById('btn-buscar');
+btnSearch.addEventListener('click', () => {
+  const inputSearch = document.getElementById('busqueda').value;
+  if (inputSearch === '' || inputSearch === ' ') {
+    alert('Escriba el nombre de un Campeón');
+  }
+  const inputValue = 'value';
+  const championBySearch = dataLol.filterBySearch(arrLolEspecifico, inputValue, inputSearch);
+  const crearCartasLol = (data) => {
+    let templateListOfCards = '';
+    data.forEach((arrLolEspecifico) => {
+      const card = `
+      <div class="d-flex flex-wrap pt-5" id="contenedor-cartas">
+      <div class="row">
+      <div class="col-sm-3 py-2">
+              <div class="card" style="width: 18rem;">
+                      <img class="card-img-top" src="${arrLolEspecifico.splash}"/>
+                      <div class="card-body" id="card-body">
+                        <h5 class="card-title" id="nombre">${arrLolEspecifico.name}</h5>
+                        <p class="card-sub-title" id="titulo">${arrLolEspecifico.title}</p>
+                        <p class="card-text"> Ataque: ${arrLolEspecifico.info.attack}</p> 
+                        <p class="card-text">Defensa: ${arrLolEspecifico.info.defense}</p>
+                        <p class="card-text">Magia: ${arrLolEspecifico.info.magic}</p>
+                        <p class="card-text">Dificultad: ${arrLolEspecifico.info.difficulty}</p>
+                        <p class="card-text">Tipo: ${arrLolEspecifico.tags}<p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                </div>     
+      `;
+      templateListOfCards += card;
+    });
+    containerCardTotal.innerHTML = templateListOfCards;
+  };
+  crearCartasLol(championBySearch);
+});
+// const buscarCampeonPorNombre = document.getElementById('buscar');
